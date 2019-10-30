7ab12b89db2c136736c4eccbf4ccdb35b3257ed6
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-10-30 19:40:04 +0100

    Comenzado ejercicio de dibujos con Canvas

---
+++ DibujoCanvas/app/src/main/java/com/example/dibujocanvas/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.dibujocanvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MiDibujo(this));
+    }
+
+    public class MiDibujo extends View {
+        public MiDibujo(Context contexto) {
+            super(contexto);
+        }
+        @Override
+        protected void onDraw(Canvas lienzo) {
+            Paint pincel = new Paint();
+            pincel.setColor(Color.BLUE);
+            pincel.setStrokeWidth(15);
+            pincel.setTextSize(50);
+            pincel.setStyle(Paint.Style.FILL);
+
+            int anchura = getWidth();
+            int altura = getHeight();
+
+            lienzo.drawCircle(150,150,80, pincel);
+            lienzo.drawText("Hola", 100, 800, pincel);
+
+        }
+    }
+}
