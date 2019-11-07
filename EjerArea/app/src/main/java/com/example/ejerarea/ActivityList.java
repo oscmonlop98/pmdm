e7cf0d5b3167460e9cfd8b387f30c37e5479d08e
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-11-07 10:10:14 +0100

    Comenzado Ejercicio Area

---
+++ EjerArea/app/src/main/java/com/example/ejerarea/ActivityList.java
@@ -0,0 +1,41 @@
+package com.example.ejerarea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityList extends AppCompatActivity {
+
+    private ListView listView;
+
+    private final String[] days = {"Circulo", "Rectangulo", "Triangulo"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, days);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Formas.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ EjerArea/app/src/main/java/com/example/ejerarea/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ejerarea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btList = findViewById(R.id.btList);
+        btList.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ActivityList.class);
+        startActivity(intent);
+    }
+
+}
