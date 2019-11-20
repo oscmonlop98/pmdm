b6b1d41212db4017820a38522018ca81ab24833c
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-11-20 08:49:05 +0100

    Ejercicio Figuras Aleatorias terminado

---
+++ FigurasAleatorias/app/src/main/java/com/example/figurasaleatorias/DrawShapes2.java
@@ -0,0 +1,13 @@
+package com.example.figurasaleatorias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrawShapes2 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_shapes2);
+    }
+}
